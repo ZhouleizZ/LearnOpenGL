dc36b4774e77c26a9ae91c27ecf25edd1cace291
Rocks <1085337601@qq.com>
2019-09-29 20:54:10 +0800

    整理

--- Source/LearnOpenGL/Camera.h
+++ Source/LearnOpenGL/Camera.h
@@ -52,8 +52,6 @@ public:
 	// Processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
 	void ProcessMouseScroll(float yoffset);
 
-	
-
 	// Calculates the front vector from the Camera's (updated) Euler Angles
 	void updateCameraVectors();
 };
